11dfa0978573723f454f7654547c37aeceb718c3
James Wang <chihyi1980@gmail.com>
2024-10-17 21:06:13 +0800

    update GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
-import Script from "next/script";
+import React, { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,38 +20,24 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-K65WHJSK'
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <html>
-      <Head>
+      <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <body>
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K65WHJSK');
-            `,
-          }}
-        />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K65WHJSK"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {children}
-      </body>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
+
